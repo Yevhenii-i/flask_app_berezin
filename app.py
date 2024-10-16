f9db1002262bd9e55b89041b5dcbfc797cd49141
Yevhenii-ie <yevhenii.berezin.22@pnu.edu.ua>
2024-10-16 16:34:16 +0300

    Add admin

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def main():  # put application's code here
     return '<h1> Hello World! </h1>', 200
 
 @app.route('/homepage')
@@ -18,9 +18,15 @@ def home():
 def greetings(name):
     name = name.upper()
     age = request.args.get('age', 0, int)
+    year = 2024 - age
 
-    return f"Welcome, {name} - {age}!"
+    return f"Welcome, {name} - {year}!"
 
+@app.route('/admin')
+def admin():
+    to_url = "/hi/administrator"
+    to_url = url_for("greetings", name="administrator", age=20, _external=True)
+    return redirect(to_url)
 
 if __name__ == '__main__':
     app.run()
