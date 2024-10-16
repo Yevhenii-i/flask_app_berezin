e56b15010dc0a74967cd56bd66dc272dc33a9497
Yevhenii-ie <yevhenii.berezin.22@pnu.edu.ua>
2024-10-16 18:16:35 +0300

    Add a lot of html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, redirect, url_for, render_template, abort
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
@@ -18,15 +18,33 @@ def home():
 def greetings(name):
     name = name.upper()
     age = request.args.get('age', 0, int)
-    year = 2024 - age
 
-    return f"Welcome, {name} - {year}!"
+
+    return render_template("hi.html", name=name, age=age)
 
 @app.route('/admin')
 def admin():
     to_url = "/hi/administrator"
     to_url = url_for("greetings", name="administrator", age=20, _external=True)
     return redirect(to_url)
+
+posts = [
+    {"id": 1, 'title': 'My First Post', 'content': 'This is the content of my first post.', 'author': 'John Doe'},
+    {"id": 2, 'title': 'Another Day', 'content': 'Today I learned about Flask macros.', 'author': 'Jane Smith'},
+    {"id": 3, 'title': 'Flask and Jinja2', 'content': 'Jinja2 is powerful for templating.', 'author': 'Mike Lee'}
+]
+
+@app.route('/posts')
+def get_posts():
+
+    return render_template("posts.html", posts=posts)
+
+@app.route('/post/<int:post_id>')
+def get_post(post_id):
+    if id > 3:
+        abort(404)
+    post = posts[post_id-1]
+    return render_template("detail_post.html", post=post)
 
 if __name__ == '__main__':
     app.run()
