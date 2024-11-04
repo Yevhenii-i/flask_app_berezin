4826472570cd7d357929406765c830eabdea4992
Yevhenii-ie <yevhenii.berezin.22@pnu.edu.ua>
2024-11-04 20:30:45 +0200

    Lab5. Added profile and login pages, cookies, sessions, flash-notifications

--- app/users/views.py
+++ app/users/views.py
@@ -1,5 +1,50 @@
 from . import users_bp
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, make_response, session, flash
+from datetime import timedelta, datetime
+
+users_info = [
+    {"username" : "admin", "password" : "admin"}
+]
+
+@users_bp.route('/profile')
+def get_profile():
+    if "username" in session:
+        cookies = []
+        for cookie in request.cookies:
+            if cookie != 'session':
+                cookies.append([cookie, request.cookies[cookie]]) # ім'я, значення
+        username_value = session["username"]
+        color_value = session["color"]
+        return render_template("profile.html", username=username_value, cookies=cookies, color=color_value)
+    return redirect(url_for("users.login"))
+
+@users_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        for user in users_info:
+            if username == user["username"] and password == user["password"]:
+                session["username"] = username
+                session["color"] = "success"
+                flash("Success: logged in successfully.", "success")
+                return redirect(url_for('users.get_profile'))
+        flash("Warning: wrong login or password.", "danger")
+    return render_template("login.html")
+
+@users_bp.route('/logout')
+def logout():
+    session.pop("username", None)
+    session.pop("age", None)
+    return redirect(url_for('users.get_profile'))
+
+@users_bp.route('/change_colors', methods=['GET', 'POST'])
+def change_colors():
+    if request.method == "POST":
+        color = request.form["color"]
+        session["color"] = color
+        return redirect(url_for('users.get_profile'))
+    return redirect(url_for('users.get_profile'))
 
 
 @users_bp.route('/homepage')
@@ -27,3 +72,62 @@ def admin():
     to_url = "/hi/administrator"
     to_url = url_for("users.greetings", name="administrator", age=20, _external=True)
     return redirect(to_url)
+
+@users_bp.route('/set_cookie')
+def set_cookie():
+    response = make_response('Кука встановлена')
+    response.set_cookie('username', 'student', max_age=timedelta(seconds=60))
+    response.set_cookie('color', '', max_age=timedelta(seconds=60))
+    return response
+
+@users_bp.route('/get_cookie')
+def get_cookie():
+    username = request.cookies.get('username')
+    return f'Користувач: {username}'
+
+
+@users_bp.route('/delete_cookie')
+def delete_cookie():
+    response = make_response('Кука видалена')
+    response.set_cookie('username', '', expires=0) # response.set_cookie('username', '', max_age=0)
+    return response
+
+
+@users_bp.route('/set_cookie_val', methods=['GET', 'POST'])
+def set_cookie_val():
+    if request.method == "POST":
+        key = request.form["cookie_key"]
+        value = request.form["cookie_value"]
+        duration = int(request.form["cookie_duration"])
+        response = make_response(redirect(url_for('users.get_profile')))
+        response.set_cookie(key, value, max_age=duration)
+        return response
+    return redirect(url_for('users.get_profile'))
+
+@users_bp.route('/delete_cookie_by_key', methods=['GET', 'POST'])
+def delete_cookie_by_key():
+    if request.method == "POST":
+        key = request.form["cookie_key"]
+        response = make_response(redirect(url_for('users.get_profile')))
+        response.set_cookie(key, '', expires=0)
+        return response
+    return redirect(url_for('users.get_profile'))
+
+@users_bp.route('/delete_all_cookies', methods=['GET', 'POST'])
+def delete_all_cookies():
+    if request.method == "POST":
+        response = make_response(redirect(url_for('users.get_profile')))
+        for cookie in request.cookies:
+            if cookie != 'session':
+                response.delete_cookie(cookie)
+        return response
+    return redirect(url_for('users.get_profile'))
+
+
+@users_bp.route('/get_all_cookies')
+def get_all_cookies():
+    #cookies = request.cookies.get_dict()
+    cookies = 0
+
+
+
