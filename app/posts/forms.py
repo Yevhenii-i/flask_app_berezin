b113088615f113fcb3517c700519d0fdff459352
Yevhenii Berezin <genjaber@gmail.com>
2024-12-01 23:32:26 +0200

    Lab6. Added database, modified other code for it to be handled accurately

--- app/posts/forms.py
+++ app/posts/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField, BooleanField, DateField, SelectField
+from wtforms import StringField, TextAreaField, SubmitField, BooleanField, DateTimeLocalField, SelectField
 from wtforms.validators import DataRequired, Length
+from datetime import datetime as dt
 
 CATEGORIES = [('tech', 'Tech'), ('scince', 'Science'), ('lifestyle', 'Lifestyle')]
 
@@ -8,7 +9,7 @@ class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired(), Length(min=1, max=100)])
     content = TextAreaField('Content', render_kw={"rows": 5, "cols": 40}, validators=[DataRequired()])
     is_active = BooleanField('Active post')
-    publish_date = DateField('Publish date', validators=[DataRequired()])
+    publish_date = DateTimeLocalField('Publish date', format="%Y-%m-%dT%H:%M", validators=[DataRequired()])
     category = SelectField('Category', choices=CATEGORIES, validators=[DataRequired()])
     submit = SubmitField('Submit')
 
--- app/posts/views.py
+++ app/posts/views.py
@@ -3,14 +3,27 @@ from flask import render_template, abort, flash, redirect, url_for, jsonify, req
 from .forms import PostForm
 import json
 
+from .models import Post
+from .. import db
+
 JSON_FILE = 'posts.json'
 
 @post_bp.route('/add_post', methods=['GET', 'POST'])
 def add_post():
     form = PostForm()
     if form.validate_on_submit():
+        new_post = Post(
+            title=form.title.data,
+            content=form.content.data,
+            is_active=form.is_active.data,
+            posted=form.publish_date.data,
+            category=form.category.data,
+            author=session.get("username", "Unknown")
+        )
+        db.session.add(new_post)
+        db.session.commit()
 
-        try:
+        """try:
             # Read existing data from the JSON file
             try:
                 with open(JSON_FILE, "r") as file:
@@ -37,26 +50,58 @@ def add_post():
                 json.dump(data, file, indent=4)
 
         except Exception as e:
-            return jsonify({"error": str(e)}), 500
+            return jsonify({"error": str(e)}), 500"""
 
         return redirect(url_for('.add_post'))
     return render_template('add_post.html', form=form)
 
 @post_bp.route('/')
 def get_posts():
-    try:
+    selres = db.select(Post).order_by(Post.posted.desc())  # Select object
+    posts = db.session.scalars(selres).all()
+
+    """try:
         with open(JSON_FILE, "r") as file:
             posts = json.load(file)
     except FileNotFoundError:
-        posts = []
+        posts = []"""
     return render_template("posts.html", posts=posts)
 
 @post_bp.route('/<int:post_id>')
 def get_post(post_id):
-    try:
+
+    selres = db.select(Post).where(Post.id == post_id)
+    post = db.session.scalar(selres)
+
+    """try:
         with open(JSON_FILE, "r") as file:
             posts = json.load(file)
     except FileNotFoundError:
         posts = []
-    post = posts[post_id]
+    post = posts[post_id]"""
     return render_template("detail_post.html", post=post)
+
+@post_bp.route('/delete/<int:post_id>')
+def delete_post(post_id):
+    Post.query.filter_by(id=post_id).delete()
+    db.session.commit()
+
+    return redirect(url_for('.get_posts'))
+
+@post_bp.route("/edit/<int:post_id>", methods=["GET", "POST"])
+def edit_post(post_id):
+    post = db.get_or_404(Post, post_id)
+    form = PostForm(obj=post)
+
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        post.is_active = form.is_active.data
+        post.publish_date = form.publish_date.data
+        post.category = form.category.data
+
+        db.session.commit()
+        flash("Post updated successfully!")
+        return redirect(url_for('.get_posts'))
+
+    return render_template("edit_post.html", form=form, post=post)
