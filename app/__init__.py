54492cf3a7ab6eb2db9ca4be6d56e14f08e2977c
Yevhenii-ie <yevhenii.berezin.22@pnu.edu.ua>
2024-12-13 01:25:16 +0200

    Lab9. Через розміщення форм на одній сторінці за одним посиланням не до кінця розумію, як правильно тестити, і в чому можуть бути помилки...

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy.orm import DeclarativeBase
+from flask_login import LoginManager
+from config import config
 
+login_manager = LoginManager()
 
 class Base(DeclarativeBase):
     pass
@@ -11,13 +14,23 @@ class Base(DeclarativeBase):
 db = SQLAlchemy(model_class=Base)
 migrate = Migrate()
 
+@login_manager.user_loader
+def load_user(user_id):
+    from app.users.models import User
+    return User.query.get(user_id)
 
-def create_app(config_name="config"):
+def create_app(config_name="default"):
     app = Flask(__name__)
-    app.config.from_object(config_name)
+    app.config.from_object(config.get(config_name))
 
     db.init_app(app)
     migrate.init_app(app, db)
+    login_manager.init_app(app)
+
+    login_manager.login_view = 'users.login'
+    login_manager.id_attribute = "get_id"
+    login_manager.login_message = 'Please log in to access this page'
+    login_manager.login_message_category = 'warning'
 
     with app.app_context():
         from . import views
